7a45c1c44b2b6a815db0f9e897bcbeba5792f65c
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-01 22:14:06 -0600

    03.01.2026 -
    create 2 new files and use getby locators to login and create new page and select the date from Calendar element.

---
+++ tests/Client App_NM.spec.js
@@ -0,0 +1,80 @@
+import {test, expect} from '@playwright/test'; // Import the test and expect functions from Playwright
+import { isGeneratorObject } from 'node:util/types';
+
+test.describe('Section 07 - Testing with Playwright', () => { 
+   test('Login to the Client App with New Method', async ({page}) => {
+     //In this test case we are using locators to automate login functionality of the client app
+     const email = "Indeewara@gmial.com";
+     const password = "Indee@123";
+     await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+     await page.getByPlaceholder("email@example.com").fill(email);
+     await page.getByPlaceholder("enter your passsword").fill(password);
+     await page.getByRole("button",{name:"Login"}).click();
+     await page.waitForLoadState('networkidle');
+     await page.locator(".card-body b").first().waitFor();
+
+     await page.locator(".card-body").filter({hasText:"ZARA COAT 3"}).
+     getByRole("button", {name:"Add To Cart"}).click();
+
+     await page.getByRole("listitem").getByRole("button", {name:"Cart"}).click();
+     await page.locator("div li").first().waitFor();
+     await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+
+     await expect(page.getByRole("button", {name:"Checkout"})).toBeVisible();
+     await page.getByRole("button", {name:"Checkout"}).click();
+
+    await page.locator(".form__cc input.text-validated").fill("4542 9931 9292 2222");
+    await page.locator("select.ddl.input:first-of-type").selectOption("08");
+    await page.locator("select.ddl.input:last-of-type").selectOption("29");
+    await page.locator('div.field.small:has-text("CVV Code") input').fill("555");
+    await page.locator('div.field:has-text("Name on Card") input').fill("Indeewara");
+
+     await page.getByPlaceholder('Select Country').pressSequentially("Can", { delay: 150 });
+     await page.getByRole("button", {name:" Canada"}).click();
+     await page.getByText("Place Order ").click();
+     await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+
+     
+   });
+});
+
+test.describe('Calanders Automation Using Playwright', () => {
+  test('New Page open and set calander', async ({browser})=> {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+
+    const monthnumber = "9";
+    const yearnumber = "2027";
+    const date = "9";
+
+    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/');
+    
+    const documentLink = page.getByRole("link", { name: "Top Deals" });
+ 
+    const [newPage]=await Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])
+   
+   await newPage.waitForLoadState('networkidle');
+   await newPage.bringToFront(page);
+   await page.pause();
+
+   await newPage.locator(".react-date-picker__inputGroup").click();
+   await newPage.locator(".react-calendar__navigation__label").click();
+   await newPage.locator(".react-calendar__navigation__label").click();
+
+   await newPage.getByText(yearnumber).click();
+   await newPage.locator(".react-calendar__year-view__months__month").nth(Number(monthnumber-1)).click();
+   await newPage.locator("//abbr[text()='"+date+"']").click();
+
+    
+
+
+  });
+
+});
+
+
--- tests/section_07.spec.js
+++ tests/section_07.spec.js
@@ -8,12 +8,17 @@ test.describe('Section 07 - Testing with Playwright', () => {
       await page.getByLabel("Employed").check();
       await page.getByLabel("gender").selectOption("Male");
    });
-test('Playwrite Special Locators - GetByPlaceholder', async ({page}) => {
-   await page.goto('https://rahulshettyacademy.com/angularpractice/');
-   await page.getByPlaceholder("Password").fill("Test");
-});
-
-
+   test('Playwrite Special Locators - GetByPlaceholder & GetbyRole', async ({page}) => {
+    expect(await page.goto('https://rahulshettyacademy.com/angularpractice/'));
+    expect(await page.getByPlaceholder("Password").fill("Test"));
+    expect(await page.getByRole("button",{name:"Submit"}).click());
+    expect(await page.getByText("Success! The Form has been submitted successfully!").isVisible());
+    await page.getByRole("Link",{name:"Shop"}).click();
+    
+    // In this care there is only one button no need to filter with name
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button",{name:"Add "}).click();
+   
+   });
 
 });// Describe the test suite for Section 07
 
