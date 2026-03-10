f54d0338e79412567b3a7dc1c3e937e250933257
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-09 23:06:47 -0500

    section_10

--- tests/section_10.spec.js
+++ tests/section_10.spec.js
@@ -1,6 +1,24 @@
-const test = require('@playwright/test')
+const {test,expect} = require('@playwright/test');
 
-test.describe('Section 10_Popup Validations', () => {
+test.describe('Section 10', () => {
+
+    test('browser navigation', async ({ page }) => {
+        await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+        //await page.goto('http://google.com');
+        //go forward and backward navigation
+        //await page.goBack();
+        //await page.goForward();
+        
+    });
+
+    test('Alert Validations', async ({ page }) => {
+        await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+
+        await expect(page.locator("#displayed-text")).toBeVisible();
+        await page.locator("#hide-textbox").click();
+        await expect(page.locator("#displayed-text")).toBeHidden();
+
+    });
 
     
 });
