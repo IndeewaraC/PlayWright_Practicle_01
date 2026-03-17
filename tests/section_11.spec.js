2eb85eadc5d1bcbbabfc14e45e5660104dcab5b1
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-16 20:03:07 -0500

    Create Orders with API

--- tests/section_11.spec.js
+++ tests/section_11.spec.js
@@ -4,8 +4,12 @@ const {test,expect,request} = require('@playwright/test');
 const loginPayload = {userEmail: "indeewaragunathilaka@gmail.com", userPassword: "Indeewara@282"};//JS object for login payload
 let token; //variable to store token from login response
 
-test.beforeAll('before all api tests',async ({})=>{
+const ordersPayload = {orders: [{country: "Cuba", productOrderedId: "6960eae1c941646b7a8b3ed3"}]};
+let orderId; //variable to store order id from create order response
 
+
+test.beforeAll('before all api tests',async ({})=>{
+//login API call to get token for authentication in subsequent API calls
 const apicontext = await request.newContext(); //create new request context for api testing
 const loginResponse = await apicontext.post("https://rahulshettyacademy.com/api/ecom/auth/login", { data: loginPayload });//send post request to login endpoint with login payload
 expect(loginResponse.ok()).toBeTruthy(); //assert that login response is successful 200 status code
@@ -14,6 +18,19 @@ const loginresponsejson = await loginResponse.json();
 token = loginresponsejson.token; //get token from login response
 console.log(token);
 
+//create Order by API call to get order id for validation in UI test
+const orderapiContext = await request.newContext(); //create new request context for api testing
+    const ordersResponse = await orderapiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
+        data: ordersPayload,
+        headers: {
+            'Authorization': token, //set authorization header with token for authentication
+            'content-Type': 'application/json' //set content type header for json payload
+        }
+    });
+    expect(ordersResponse.ok()).toBeTruthy();//assert that create order response is successful 200 status code
+    const ordersResponseJson = await ordersResponse.json();
+    console.log(ordersResponseJson);
+    orderId = ordersResponseJson.orders[0];
 });
 
 
@@ -25,28 +42,33 @@ test.describe('API testing',()=>{
      window.localStorage.setItem('token', value); //set token in to the local storage of the browser before the page loads
 
     }, token);//pass token as value to the init script
-await page.goto("https://rahulshettyacademy.com/client/");//navigate to the client app
-
-     await page.locator(".card-body").filter({hasText:"ZARA COAT 3"}).
-     getByRole("button", {name:"Add To Cart"}).click();
-
-     await page.getByRole("listitem").getByRole("button", {name:"Cart"}).click();
-     await page.locator("div li").first().waitFor();
-     await expect(page.getByText("ZARA COAT 3")).toBeVisible();
-
-     await expect(page.getByRole("button", {name:"Checkout"})).toBeVisible();
-     await page.getByRole("button", {name:"Checkout"}).click();
-
-    await page.locator(".form__cc input.text-validated").fill("4542 9931 9292 2222");
-    await page.locator("select.ddl.input:first-of-type").selectOption("08");
-    await page.locator("select.ddl.input:last-of-type").selectOption("29");
-    await page.locator('div.field.small:has-text("CVV Code") input').fill("555");
-    await page.locator('div.field:has-text("Name on Card") input').fill("Indeewara");
-
-     await page.getByPlaceholder('Select Country').pressSequentially("Can", { delay: 150 });
-     await page.getByRole("button", {name:" Canada"}).click();
-     await page.getByText("Place Order ").click();
-     await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+    await page.goto("https://rahulshettyacademy.com/client/");//navigate to the client app
    });
-    
+
+ test('validate order in UI', async ({page}) => {
+
+    await page.addInitScript(value => {
+     window.localStorage.setItem('token', value); //set token in to the local storage of the browser before the page loads
+
+    }, token);//pass token as value to the init script
+    await page.goto("https://rahulshettyacademy.com/client/");//navigate to the client app
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const orderRows = await page.locator("tbody tr");
+
+        for(let i =0; i<await orderRows.count(); ++i)
+        {
+            const rowOrderId =await orderRows.nth(i).locator("th").textContent();
+            if (orderId.includes(rowOrderId))
+                {
+                await orderRows.nth(i).locator("button").first().click();
+                break;
+                }
+        }
+
+const orderIdDetails =await page.locator(".col-text").textContent();
+expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+   });
+
 });
