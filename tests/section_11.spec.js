976d44947823ae693d869a60dc5cd480c6fd12c1
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-16 20:54:25 -0500

    API Util file and class created

--- tests/section_11.spec.js
+++ tests/section_11.spec.js
@@ -1,56 +1,34 @@
 const {test,expect,request} = require('@playwright/test');
+const {APIutils} = require('../utils/APIutils');
+
 //api testing from playwright
-
+let response ;
 const loginPayload = {userEmail: "indeewaragunathilaka@gmail.com", userPassword: "Indeewara@282"};//JS object for login payload
-let token; //variable to store token from login response
-
 const ordersPayload = {orders: [{country: "Cuba", productOrderedId: "6960eae1c941646b7a8b3ed3"}]};
-let orderId; //variable to store order id from create order response
 
-
-test.beforeAll('before all api tests',async ({})=>{
-//login API call to get token for authentication in subsequent API calls
-const apicontext = await request.newContext(); //create new request context for api testing
-const loginResponse = await apicontext.post("https://rahulshettyacademy.com/api/ecom/auth/login", { data: loginPayload });//send post request to login endpoint with login payload
-expect(loginResponse.ok()).toBeTruthy(); //assert that login response is successful 200 status code
-
-const loginresponsejson = await loginResponse.json();
-token = loginresponsejson.token; //get token from login response
-console.log(token);
-
-//create Order by API call to get order id for validation in UI test
-const orderapiContext = await request.newContext(); //create new request context for api testing
-    const ordersResponse = await orderapiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
-        data: ordersPayload,
-        headers: {
-            'Authorization': token, //set authorization header with token for authentication
-            'content-Type': 'application/json' //set content type header for json payload
-        }
-    });
-    expect(ordersResponse.ok()).toBeTruthy();//assert that create order response is successful 200 status code
-    const ordersResponseJson = await ordersResponse.json();
-    console.log(ordersResponseJson);
-    orderId = ordersResponseJson.orders[0];
+test.beforeAll(async () =>
+{
+    const apicontxt = await request.newContext(); 
+    const apiutil= new APIutils(apicontxt,loginPayload);//create new request context for api testing
+    response = await apiutil.createOrder(ordersPayload);//create order by API call to get order id for validation in UI test and store in response.
 });
-
 
 test.describe('API testing',()=>{
 
- test('place the order', async ({page}) => {
-
+ test('By Pass login page with API', async ({page}) => {
     await page.addInitScript(value => {
      window.localStorage.setItem('token', value); //set token in to the local storage of the browser before the page loads
 
-    }, token);//pass token as value to the init script
+    }, response.token);//pass token as value to the init script
     await page.goto("https://rahulshettyacademy.com/client/");//navigate to the client app
    });
 
  test('validate order in UI', async ({page}) => {
-
     await page.addInitScript(value => {
      window.localStorage.setItem('token', value); //set token in to the local storage of the browser before the page loads
 
-    }, token);//pass token as value to the init script
+    }, response.token);//pass token as value to the init script
+
     await page.goto("https://rahulshettyacademy.com/client/");//navigate to the client app
     await page.locator("button[routerlink*='myorders']").click();
     await page.locator("tbody").waitFor();
@@ -59,7 +37,7 @@ test.describe('API testing',()=>{
         for(let i =0; i<await orderRows.count(); ++i)
         {
             const rowOrderId =await orderRows.nth(i).locator("th").textContent();
-            if (orderId.includes(rowOrderId))
+            if (response.orderId.includes(rowOrderId))
                 {
                 await orderRows.nth(i).locator("button").first().click();
                 break;
@@ -67,7 +45,7 @@ test.describe('API testing',()=>{
         }
 
 const orderIdDetails =await page.locator(".col-text").textContent();
-expect(orderId.includes(orderIdDetails)).toBeTruthy();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
    });
 
---
+++ utils/APIutils.js
@@ -0,0 +1,42 @@
+class APIutils
+{
+
+    constructor(apicontxt,loginPayload)
+    {
+        this.apiContext = apicontxt;
+        this.loginPayload = loginPayload;
+    }
+
+    async getToken()
+    {
+    const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", { data: this.loginPayload });//send post request to login endpoint with login payload
+    
+    const loginresponsejson = await loginResponse.json();
+    const token = loginresponsejson.token; //get token from login response
+    console.log(token);
+    return token;
+    }
+
+    async createOrder(ordersPayload)
+    {
+        let response = {}; //js object to store response from create order api call
+       response.token = await this.getToken(); //get token by calling getToken method to set authorization header for create order api call
+        //create Order by API call to get order id for validation in UI test
+            const ordersResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
+                data: ordersPayload,
+                headers: {
+                    'Authorization': response.token, //set authorization header with token for authentication
+                    'content-Type': 'application/json' //set content type header for json payload
+                }
+            });
+            const ordersResponseJson = await ordersResponse.json();
+            console.log(ordersResponseJson);
+            const orderId = ordersResponseJson.orders[0];
+            response.orderId = orderId;//get order id from create order response
+
+            return response;
+    }
+
+}
+
+module.exports = {APIutils};//this is the way to export a class in nodejs so that it can be imported in other files using require()
