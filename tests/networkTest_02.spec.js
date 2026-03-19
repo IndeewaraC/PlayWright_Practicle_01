925c2a35303b200a4b0057f7825cfeee8af42bee
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-19 18:20:30 -0500

    section_12

--- tests/networkTest_02.spec.js
+++ tests/networkTest_02.spec.js
@@ -14,12 +14,31 @@ test('Security test request interception', async ({ page }) => {
     window.localStorage.setItem('token', value);
 
   }, response);
+  page.route('**/*.css', route => route.abort());
+  page.route('**/*.png', route => route.abort());
   await page.goto("https://rahulshettyacademy.com/client/");
   await page.locator("button[routerlink*='myorders']").click();
+  //this following lines will listen to the request and response and print the url and status code in console
+  page.on('request', request => console.log(request.url()));
+  page.on('response', response => console.log(response.url(), response.status()));
+  await page.pause();
   await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
     route =>
       route.continue({ url: "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=621661f884b053f6765465b6" })
   )
   await page.locator("button:has-text('view')").first().click();
   await expect(page.locator("p").last()).toHaveText("You are not authorize to view this order");
+
+
+
+});
+test('server test request interception', async ({ browser }) => {
+  const brcontxt = await browser.newContext();
+  const page = await brcontxt.newPage();
+  page.route('**/*.css', route => route.abort()); //it will stop the call reach browser
+  page.route('**/*.js', route => route.abort()); //it will stop the call reach browser
+  await page.goto("https://rahulshettyacademy.com/client/");
+  page.on('request', request => console.log(request.url()));
+  page.on('response', response => console.log(response.url(), response.status()));
+  await page.pause();
 });
